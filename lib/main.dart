e510b71d5fae2234ddccdff9426745c9eb688caf
janCarlos <202000673J@utea.edu.pe>
2023-09-22 08:03:55 -0500

    calcual el imc

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
   @override
   State<MyApp> createState() => _MyAppState();
 }
@@ -11,6 +11,14 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   double valueRedSlider_peso = 0;
   double valueRedSlider_altura = 0;
+  double imc = 0;
+
+  void calcularIMC() {
+    double peso = valueRedSlider_peso;
+    double altura = valueRedSlider_altura / 100;
+    imc = peso / (altura * altura);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,30 +29,53 @@ class _MyAppState extends State<MyApp> {
           title: const Text('IMC APP'),
         ),
         body: Center(
-            child: Column(
-          children: [
-            Slider(
-              min: 0,
-              max: 255,
-              value: valueRedSlider_peso,
-              onChanged: (value) {
-                print(value);
-                valueRedSlider_peso = value;
-                setState(() {});
-              },
-            ),
-            Slider(
-              min: 0,
-              max: 255,
-              value: valueRedSlider_altura,
-              onChanged: (value) {
-                print(value);
-                valueRedSlider_altura = value;
-                setState(() {});
-              },
-            )
-          ],
-        )),
+          child: Column(
+            children: [
+              Slider(
+                min: 0,
+                max: 250,
+                value: valueRedSlider_peso,
+                onChanged: (value) {
+                  print(value);
+                  setState(() {
+                    valueRedSlider_peso = value;
+                    calcularIMC();
+                  });
+                },
+              ),
+              Text('Peso: ${valueRedSlider_peso.toStringAsFixed(0)} Kg'),
+              Slider(
+                min: 0,
+                max: 200,
+                value: valueRedSlider_altura,
+                onChanged: (value) {
+                  print(value);
+                  setState(() {
+                    valueRedSlider_altura = value;
+                    calcularIMC();
+                  });
+                },
+              ),
+              Text('Altura: ${valueRedSlider_altura.toStringAsFixed(0)} cm'),
+              const SizedBox(
+                height: 30.0,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  calcularIMC();
+                },
+                child: Text('CALCULAR'),
+              ),
+              const SizedBox(
+                height: 30.0,
+              ),
+              Text(
+                'IMC: ${imc.toStringAsFixed(2)}',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
